81da64d17deb2b6e7f44c57532c97f55db5d1084
smzkjnpi3232 <kizmhny2561@gmail.com>
2024-12-30 01:59:06 +0900

    Q4 配列 × オブジェクトの解答を記述

--- question/app.js
+++ question/app.js
@@ -20,3 +20,24 @@ let user = {
 };
 
 console.log(user.age);
+
+// q4
+let playerList = [
+  {
+    name: 'John',
+    age: 26,
+    favorites: ['Card Game', 'Basket Ball', 'Programming'],
+  },
+  {
+    name: 'Bob',
+    age: 33,
+    favorites: ['Tinder', 'The Legend of Zelda'],
+  },
+  {
+    name: 'Michael',
+    age: 22,
+    favorites: ['Football', 'Smash Bros.'],
+  },
+];
+
+console.log(playerList[1].favorites[1]);
