03df067aeeb6fb27929c411a0df1c49bb021c6de
smzkjnpi3232 <kizmhny2561@gmail.com>
2024-12-30 01:42:28 +0900

    Q2配列の解答を記述

--- question/app.js
+++ question/app.js
@@ -1,4 +1,12 @@
+// q1
 let nickname = 'じゅんじゅん';
 let age = 30;
 
 console.log('私のニックネームは' + nickname + 'です。年齢は' + age + '歳です。');
+
+// q2
+let languages = ['JavaScript', 'PHP', 'Ruby', 'Python', 'Go'];
+let javaScript = languages[0];
+let python = languages[3];
+
+console.log(`私の好きな言語は${javaScript}です。次は${python}を勉強してみたいです。`);
