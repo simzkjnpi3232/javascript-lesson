250d874f6885663939f4971dd955ed0fc66f90bb
smzkjnpi3232 <kizmhny2561@gmail.com>
2024-12-30 13:25:37 +0900

    Q1 標準組み込みオブジェクトの解答を記述

--- question/app.js
+++ question/app.js
@@ -1,17 +1,17 @@
-// q1
+// Q1 変数
 let nickname = 'じゅんじゅん';
 let age = 30;
 
 console.log('私のニックネームは' + nickname + 'です。年齢は' + age + '歳です。');
 
-// q2
+// Q2 配列
 let languages = ['JavaScript', 'PHP', 'Ruby', 'Python', 'Go'];
 let javaScript = languages[0];
 let python = languages[3];
 
 console.log(`私の好きな言語は${javaScript}です。次は${python}を勉強してみたいです。`);
 
-//q3
+// Q3 オブジェクト
 let user = {
   name: 'John',
   age: 26,
@@ -25,7 +25,7 @@ let user = {
 
 console.log(user.age);
 
-// q4
+// Q4 配列 × オブジェクト
 let playerList = [
   {
     name: 'John',
@@ -46,14 +46,14 @@ let playerList = [
 
 console.log(playerList[1].favorites[1]);
 
-// q5
+// Q5 四則演算
 let johnAge = playerList[0].age;
 let bobAge = playerList[1].age;
 let michaelAge = playerList[2].age;
 
 console.log((johnAge + bobAge + michaelAge) / 3);
 
-// q6
+// Q6 関数
 function sayHello() {
   console.log('Hello');
 }
@@ -66,10 +66,10 @@ const sayWorld = function() {
 
 sayWorld();
 
-// q7
+// Q7 メソッド
 user.sayHello();
 
-// q8
+// Q8 引数
 let calc = {
   add: function(x, y) {
     console.log(x + y);
@@ -90,16 +90,20 @@ calc.subtract(20, 10);
 calc.multiply(7, 7);
 calc.divide(10, 2);
 
-// q9
+// Q9 返り値
 function remainder(x, y) {
   return console.log(x + 'を' + y + 'で割った余りは' + x % y + 'です。');
 }
 
 remainder(5, 3);
 
-// q10
-function foo() {
-  let x = 1;
-}
-console.log(x);
+// Q10 スコープ
+// function foo() {
+//   let x = 1;
+// }
+// console.log(x);
 // エラーが出力される理由は、console.log(x)が変数xをスコープの外から参照しようとしているためです。
+
+// Q1 標準組み込みオブジェクト
+let randomInteger = Math.floor(Math.random() * 10);
+console.log(randomInteger);
